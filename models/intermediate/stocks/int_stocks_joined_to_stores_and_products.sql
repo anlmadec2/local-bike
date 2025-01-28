ad172efa5b9f6a85a44e874dc576f617aa18c44b
Anaël Madec <anael.madec@keyrus.com>
2025-01-28 19:16:51 +0000

    FEAT : int layer

---
+++ models/intermediate/stocks/int_stocks_joined_to_stores_and_products.sql
@@ -0,0 +1,11 @@
+SELECT stocks.stock_id,
+    stocks.store_id, 
+    stores.store_name,
+    stocks.product_id, 
+    p.product_name,
+    p.category_name,
+    p.brand_name,
+    stocks.quantity as stock_quantity
+FROM {{ ref('stg_local_bike__stocks') }} AS stocks
+    INNER JOIN {{ ref('stg_local_bike__stores') }} AS stores on stocks.store_id = stores.store_id
+    INNER JOIN {{ ref('int_products_joined_to_category_and_brand') }} AS p on products.p = stocks.product_id
